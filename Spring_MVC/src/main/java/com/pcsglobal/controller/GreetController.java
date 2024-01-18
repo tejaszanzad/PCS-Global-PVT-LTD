40b096959a327e3c0e681547df373b4b6bc38a2d
Tejas Zanzad <tejaszanzad90@gmail.com>
2024-01-18 19:16:58 +0530

    Spring_MVC added

---
+++ Spring_MVC/src/main/java/com/pcsglobal/controller/GreetController.java
@@ -0,0 +1,19 @@
+package com.pcsglobal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class GreetController {
+	
+	@GetMapping("/greet")
+	public String greet(Model model, @RequestParam(name="name", required=false, defaultValue="World!") String name) {
+
+		model.addAttribute("name", name);
+
+		return "hello";
+	}
+}
